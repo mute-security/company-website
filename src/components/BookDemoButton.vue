14d2de2129053d6d027526e7bfe7aa5c8f10d8fc
Jossef Kadouri <jossef12@gmail.com>
2025-11-20 12:15:29 +0200

    fix: broken link

--- src/components/BookDemoButton.vue
+++ src/components/BookDemoButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="button-wrapper">
-    <a href="https://calendly.com/zajogu/intro" target="_blank" class="book-demo-button">
+    <a href="https://calendly.com/sahar-mutesecurity/30min" target="_blank" class="book-demo-button">
       <span class="button-text">Schedule a call</span>
       <span class="button-icon">→</span>
     </a>
